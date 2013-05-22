0da1ef75dba4f1b5a4ecdbd6d32c03d6941a5bb9
Izohor <izohoro@gmail.com>
2013-05-22 16:21:50 +0400

    add labs

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QtGui/QApplication>
+#include "mathparse.h"
+
+int main(int argc, char *argv[])
+{
+    //
+    if (i<90)
+        //
+    QApplication a(argc, argv);
+    MathParse w;
+    w.show();
+
+    return a.exec();
+}
